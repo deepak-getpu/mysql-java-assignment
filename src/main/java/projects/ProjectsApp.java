cec0afec647e94d75082db3a06bd5e6b21dde08f
deepak-getpu <deepak.getpu@gmail.com>
2024-01-13 15:57:26 -0500

    first version

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+
+	public static void main(String[] args) {
+		
+		DbConnection.getConnection();
+		
+
+	}
+
+}
---
+++ src/main/java/projects/exception/DbExceptionn.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package projects.exception;
+
+/**
+ * 
+ */
+@SuppressWarnings("serial")
+public class DbExceptionn extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	public DbExceptionn() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public DbExceptionn(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public DbExceptionn(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public DbExceptionn(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
